49da26112ab0bb85b94464e9acffc294eff87bac
Kwak-mochi <163822461+Kwak-mochi@users.noreply.github.com>
2024-03-25 14:10:25 +0900

    hypotension

---
+++ Hypotension/cohort/Microsoft SQL Server/cdm_cohort.sql
@@ -0,0 +1,146 @@
+--concept id
+--3020891 : BT, body temperature
+--9201 : 입원
+--19112656 : AAP1I
+--21035250 : AAPI
+--43291914 : AAP5I
+--2028218, 2028220 : PAAPI
+--4171047 : IV
+
+CREATE TABLE cdm_t1 AS
+SELECT aa.*
+FROM
+    (SELECT a.person_id,
+            a.visit_occurrence_id,
+            a.VISIT_START_DATETIME,
+            a.VISIT_END_DATETIME,
+            b.measurement_datetime,
+            c.drug_exposure_start_datetime,
+            c.drug_concept_id,
+            c.drug_source_value,
+            RANK() OVER (PARTITION BY a.person_id ORDER BY c.drug_exposure_start_datetime, b.measurement_datetime) AS rank
+     FROM (SELECT person_id,
+                  visit_occurrence_id,
+                  VISIT_START_DATETIME,
+                  VISIT_END_DATETIME
+           FROM CDM_2021.origin_VISIT_occurrence
+           WHERE visit_concept_id = '9201') a
+          JOIN (SELECT person_id,
+                       visit_occurrence_id,
+                       measurement_datetime
+                FROM CDM_2021.origin_MEASUREMENT
+                WHERE MEASUREMENT_CONCEPT_ID = '3020891'
+                  AND VALUE_AS_NUMBER > 37.1) b ON a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+                                                  AND a.VISIT_START_DATETIME < b.measurement_datetime
+                                                  AND b.measurement_datetime < a.VISIT_END_DATETIME
+          JOIN (SELECT person_id,
+                       visit_occurrence_id,
+                       drug_exposure_start_datetime,
+                       drug_concept_id,
+                       drug_source_value
+                FROM CDM_2021.origin_DRUG_EXPOSURE
+                WHERE ROUTE_CONCEPT_ID = '4171047'
+                  AND stop_reason IS NULL
+                  AND drug_exposure_start_datetime > '2011-12-31'
+                  AND drug_concept_id IN ('19112656',
+                                           '21035250',
+                                           '43291914',
+                                           '2028218',
+                                           '2028220')) c ON b.VISIT_OCCURRENCE_ID = c.VISIT_OCCURRENCE_ID
+                                                              AND b.measurement_datetime < c.drug_exposure_start_datetime
+                                                              AND c.drug_exposure_start_datetime - b.measurement_datetime < '06:00:00') aa
+          JOIN cdm_2021.origin_person bb ON aa.PERSON_ID = bb.person_id
+                                          AND DATEPART(year, aa.drug_exposure_start_datetime) - bb.year_of_birth > 19
+          JOIN cdm_vital cc ON aa.VISIT_OCCURRENCE_ID = cc.VISIT_OCCURRENCE_ID
+                             AND aa.drug_exposure_start_datetime < cc.measurement_datetime
+                             AND cc.measurement_datetime - aa.drug_exposure_start_datetime < '06:00:00'
+                             AND (cc.mbp IS NOT NULL OR cc.sbp IS NOT NULL)
+WHERE aa.rank = 1;
+
+-- cdm_t2 테이블 생성
+--concept id
+--43290740, 19076658, 42918864 : Dobutamine
+--1337860, 35606561, 42799673, 35606558, 36412218, 42918966, 1337860 : Dopamine
+--44078900, 36419168, 36419192 : Epinephrine
+--21058703, 1321363 : Norepinephrine
+--1507835 : Vasopressin
+
+CREATE TABLE cdm_t2 AS
+SELECT
+    CASE
+        WHEN dd.drug_concept_id_bf IS NULL AND ee.drug_concept_id_af IS NOT NULL THEN 'hypo'
+        WHEN SUBSTRING(dd.drug_source_value_bf, 1, 3) = SUBSTRING(ee.drug_source_value_af, 1, 3) AND dd.quantity_bf < ee.quantity_af THEN 'hypo'
+        WHEN cc.sbp_af < 90 THEN 'hypo'
+        WHEN cc.mbp_af < 65 THEN 'hypo'
+        WHEN bb.sbp_bf - cc.sbp_af > 29 THEN 'hypo'
+        ELSE 'control'
+    END AS group_hypo,
+    aa.person_id,
+    aa.visit_occurrence_id,
+    aa.visit_start_datetime,
+    aa.visit_end_datetime,
+    aa.measurement_datetime,
+    aa.drug_exposure_start_datetime,
+    aa.drug_concept_id,
+    aa.drug_source_value,
+    bb.measurement_datetime_bf,
+    bb.sbp_bf,
+    bb.dbp_bf,
+    bb.mbp_bf,
+    cc.measurement_datetime_af,
+    cc.sbp_af,
+    cc.dbp_af,
+    cc.mbp_af,
+    dd.drug_concept_id_bf,
+    dd.drug_source_value_bf,
+    dd.quantity_bf,
+    ee.drug_concept_id_af,
+    ee.drug_source_value_af,
+    ee.quantity_af
+FROM
+    (SELECT DISTINCT
+            person_id,
+            visit_occurrence_id,
+            visit_start_datetime,
+            visit_end_datetime,
+            measurement_datetime,
+            drug_exposure_start_datetime,
+            drug_concept_id,
+            drug_source_value,
+            RANK() OVER (PARTITION BY person_id ORDER BY drug_source_value) AS rank
+     FROM cdm_t1) aa
+          LEFT JOIN (SELECT DISTINCT
+                            a.person_id,
+                            b.visit_occurrence_id,
+                            b.measurement_datetime AS measurement_datetime_bf,
+                            b.sbp AS sbp_bf,
+                            b.dbp AS dbp_bf,
+                            b.mbp AS mbp_bf,
+                            RANK() OVER (PARTITION BY a.person_id ORDER BY b.measurement_datetime DESC) AS rank
+                     FROM cdm_t1 a
+                              JOIN cdm_vital b ON a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+                                                 AND b.measurement_datetime < a.drug_exposure_start_datetime
+                                                 AND a.drug_exposure_start_datetime - b.measurement_datetime < '06:00:00'
+                                                 AND (b.mbp IS NOT NULL OR b.sbp IS NOT NULL)) bb ON aa.person_id = bb.person_id
+                                                                                                         AND bb.rank = 1
+          JOIN (SELECT DISTINCT
+                            a.person_id,
+                            b.visit_occurrence_id,
+                            b.measurement_datetime AS measurement_datetime_af,
+                            b.sbp AS sbp_af,
+                            b.dbp AS dbp_af,
+                            b.mbp AS mbp_af,
+                            RANK() OVER (PARTITION BY a.person_id ORDER BY b.measurement_datetime) AS rank
+                     FROM cdm_t1 a
+                              JOIN cdm_vital b ON a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+                                                 AND a.drug_exposure_start_datetime < b.measurement_datetime
+                                                 AND b.measurement_datetime - a.drug_exposure_start_datetime < '06:00:00'
+                                                 AND (b.mbp IS NOT NULL OR b.sbp IS NOT NULL)) cc ON aa.person_id = cc.person_id
+                                                                                                         AND cc.rank = 1
+          LEFT JOIN (SELECT DISTINCT
+                            a.person_id,
+                            b.visit_occurrence_id,
+                            b.drug_concept_id AS drug_concept_id_bf,
+                            b.drug_source_value AS drug_source_value_bf,
+                            b.quantity AS quantity_bf,
+                            RANK() OVER (PARTITION BY a.person_id ORDER BY b.drug_exposure_start_datetime DESC, b.quantity DESC, b.drug_concept_id) AS rank
---
+++ Hypotension/cohort/PostgreSQL/cdm_cohort.sql
@@ -0,0 +1,154 @@
+--concept id
+--3020891 : BT, body temperature
+--9201 : 입원
+--19112656 : AAP1I
+--21035250 : AAPI
+--43291914 : AAP5I
+--2028218, 2028220 : PAAPI
+--4171047 : IV
+
+create table cdm_t1 as (
+    select distinct aa.* from
+    (
+        select distinct a.person_id, a.visit_occurrence_id, a.VISIT_START_DATETIME, a.VISIT_END_DATETIME,
+        b.measurement_datetime, c.drug_exposure_start_datetime, c.drug_concept_id, c.drug_source_value,
+        rank()over(partition by a.person_id order by c.drug_exposure_start_datetime, b.measurement_datetime) rank
+        
+        -- 입원
+        from ( 
+            select person_id, visit_occurrence_id, VISIT_START_DATETIME, VISIT_END_DATETIME
+            from CDM_2021.origin_VISIT_occurrence
+            where visit_concept_id='9201'
+        ) a
+        
+         --발열이 있던 환자
+        join (
+            select person_id, visit_occurrence_id, measurement_datetime
+            from CDM_2021.origin_MEASUREMENT
+            where MEASUREMENT_CONCEPT_ID='3020891' and  VALUE_AS_NUMBER>37.1
+        ) b
+        on a.VISIT_OCCURRENCE_ID=b.VISIT_OCCURRENCE_ID
+        and a.VISIT_START_DATETIME < b.measurement_datetime
+        and b.measurement_datetime < a.VISIT_END_DATETIME
+        
+        --발열이후 6시간 이내 AAP, PAA를 IV투여가 된 환자
+        join (
+            select person_id, visit_occurrence_id, drug_exposure_start_datetime, drug_concept_id, drug_source_value
+            from CDM_2021.origin_DRUG_EXPOSURE
+            where ROUTE_CONCEPT_ID = '4171047' and stop_reason is null
+            and drug_exposure_start_datetime > '2011-12-31'
+            and drug_concept_id in ('19112656', '21035250', '43291914', '2028218', '2028220')
+        ) c
+        on b.VISIT_OCCURRENCE_ID=c.VISIT_OCCURRENCE_ID
+        and b.measurement_datetime < c.drug_exposure_start_datetime
+        and c.drug_exposure_start_datetime - b.measurement_datetime < '06:00:00'
+    ) aa
+    
+    --성인
+    join cdm_2021.origin_person bb 
+    on aa.PERSON_ID = bb.person_id
+    and EXTRACT(year from aa.drug_exposure_start_datetime)-bb.year_of_birth  > 19
+    
+    --index약물 이후 6시간 이내 혈압측정
+    join cdm_vital cc 
+    on aa.VISIT_OCCURRENCE_ID = cc.VISIT_OCCURRENCE_ID
+    and aa.drug_exposure_start_datetime < cc.measurement_datetime
+    and cc.measurement_datetime - aa.drug_exposure_start_datetime < '06:00:00'
+    and (cc.mbp is not null or cc.sbp is not null)
+    
+    --환자당 첫번째 사건
+    where aa.rank=1
+    
+)
+
+
+
+
+--concept id
+--43290740, 19076658, 42918864 : Dobutamine
+--1337860, 35606561, 42799673, 35606558, 36412218, 42918966, 1337860 : Dopamine
+--44078900, 36419168, 36419192 : Epinephrine
+--21058703, 1321363 : Norepinephrine
+--1507835 : Vasopressin
+create table cdm_t2 as ( 
+    select 
+    case when dd.drug_concept_id_bf is null and ee.drug_concept_id_af is not null then 'hypo'
+    when substr(dd.drug_source_value_bf,1,3)=substr(ee.drug_source_value_af,1,3) and dd.quantity_bf < ee.quantity_af then 'hypo'
+    when cc.sbp_af < 90 then 'hypo'
+    when cc.mbp_af < 65 then 'hypo'
+    when bb.sbp_bf-cc.sbp_af > 29 then 'hypo'
+    else 'control' end as group_hypo,
+    
+    aa.person_id, aa.visit_occurrence_id,
+    aa.visit_start_datetime, aa.visit_end_datetime, aa.measurement_datetime, aa.drug_exposure_start_datetime,
+    aa.drug_concept_id, aa.drug_source_value,
+    bb.measurement_datetime_bf, bb.sbp_bf, bb.dbp_bf, bb.mbp_bf,
+    cc.measurement_datetime_af, cc.sbp_af, cc.dbp_af, cc.mbp_af,
+    dd.drug_concept_id_bf, dd.drug_source_value_bf, dd.quantity_bf,
+    ee.drug_concept_id_af, ee.drug_source_value_af, ee.quantity_af
+    
+    --환자당 첫번째 사건(약물)
+    from (
+        select distinct person_id, visit_occurrence_id, 
+        visit_start_datetime, visit_end_datetime, measurement_datetime, drug_exposure_start_datetime,
+        drug_concept_id, drug_source_value,
+        rank()over(partition by person_id order by drug_source_value) rank 
+        from cdm_t1
+    ) aa
+    
+    --index약물 이전 6시간 이내 혈압측정 중 마지막 값
+    left join (select distinct a.person_id, b.visit_occurrence_id, b.measurement_datetime measurement_datetime_bf,
+          b.sbp sbp_bf, b.dbp dbp_bf, b.mbp mbp_bf,
+          rank()over(partition by a.person_id order by b.measurement_datetime desc) rank
+          from cdm_t1 a
+          join cdm_vital b
+         on a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+         and b.measurement_datetime < a.drug_exposure_start_datetime
+         and a.drug_exposure_start_datetime - b.measurement_datetime < '06:00:00'
+         and (b.mbp is not null or b.sbp is not null)) bb
+    on aa.person_id = bb.person_id
+    and bb.rank=1
+    
+    --index약물 이후 6시간 이내 혈압측정 중 첫번째 값
+    join (select distinct a.person_id, b.visit_occurrence_id, b.measurement_datetime measurement_datetime_af,
+          b.sbp sbp_af, b.dbp dbp_af, b.mbp mbp_af,
+          rank()over(partition by a.person_id order by b.measurement_datetime) rank
+          from cdm_t1 a
+          join cdm_vital b
+         on a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+         and a.drug_exposure_start_datetime < b.measurement_datetime
+         and b.measurement_datetime - a.drug_exposure_start_datetime < '06:00:00'
+         and (b.mbp is not null or b.sbp is not null)) cc
+    on aa.person_id = cc.person_id
+    and cc.rank=1
+    
+    --index약물 이전 24시간 이내 승압제 iv 중 마지막
+    left join (select distinct a.person_id, b.visit_occurrence_id,
+          b.drug_concept_id drug_concept_id_bf, b.drug_source_value drug_source_value_bf, b.quantity quantity_bf,
+          rank()over(partition by a.person_id order by b.drug_exposure_start_datetime desc, b.quantity desc, b.drug_concept_id) rank
+          from cdm_t1 a
+          join CDM_2021.origin_DRUG_EXPOSURE b
+          on a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+          and b.drug_exposure_start_datetime < a.drug_exposure_start_datetime
+          and a.drug_exposure_start_datetime - b.drug_exposure_start_datetime < '24:00:00'
+          and b.drug_concept_id in(43290740, 19076658, 42918864, 1337860, 35606561, 42799673, 35606558, 36412218, 
+                                 42918966, 1337860, 44078900, 36419168, 36419192, 21058703, 1321363, 1507835)) dd
+    on aa.person_id = dd.person_id
+    and dd.rank=1
+    
+    --index약물 이후 24시간 이내 승압제 iv 중 처음
+    left join (select distinct a.person_id, b.visit_occurrence_id,
+          b.drug_concept_id drug_concept_id_af, b.drug_source_value drug_source_value_af, b.quantity quantity_af,
+          rank()over(partition by a.person_id order by b.drug_exposure_start_datetime, b.quantity desc, b.drug_concept_id) rank
+          from cdm_t1 a
+          join CDM_2021.origin_DRUG_EXPOSURE b
+          on a.VISIT_OCCURRENCE_ID = b.VISIT_OCCURRENCE_ID
+          and a.drug_exposure_start_datetime < b.drug_exposure_start_datetime
+          and b.drug_exposure_start_datetime - a.drug_exposure_start_datetime < '24:00:00'
+          and b.drug_concept_id in(43290740, 19076658, 42918864, 1337860, 35606561, 42799673, 35606558, 36412218, 
+                                 42918966, 1337860, 44078900, 36419168, 36419192, 21058703, 1321363, 1507835)) ee
+    on aa.person_id = ee.person_id
+    and ee.rank=1
+
+    where aa.rank=1
+)
